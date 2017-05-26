790e74cd6989a8b34f2beee8b57dfc84724755ba
brijesh-pant <pantbrijesh717@live.com>
2017-05-26 10:24:32 +0530

    development strategy using webpack-dev-middleware and react-hot-loader

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
+import path from 'path'
 import express from 'express'
+import webpack from 'webpack'
+import config from '../webpack.config'
 
 import { render } from '../src/server'
 
 const app = express()
+const compiler = webpack(config)
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath,
+  stats: {
+    colors: true
+  },
+  historyApiFallback: true
+}))
+
+app.use(require('webpack-hot-middleware')(compiler))
 
 app.use(express.static('dist'))
 
